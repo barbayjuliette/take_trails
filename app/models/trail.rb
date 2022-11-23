415137202cd6b09401aa9a5b145729bc113721c9
Nurraihan Jalil <nurraihanjalilx@gmail.com>
2022-11-23 16:06:03 +0800

    filter on difficulties -add global var

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,8 +1,9 @@
 class Trail < ApplicationRecord
+  DIFFICULTIES = %w(Easy Intermediate Difficult)
   has_many :trips
 
   validates :name, presence: true, uniqueness: true
-  validates :difficulty, presence: true, inclusion: { in: %w(Easy Intermediate Difficult)}
+  validates :difficulty, presence: true, inclusion: { in: DIFFICULTIES }
   validates :description, presence: true, length: { minimum: 5 }
   validates :distance, presence: true, numericality: true
   validates :duration, presence: true, numericality: true
@@ -15,3 +16,5 @@ class Trail < ApplicationRecord
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
 end
+
+# Trail::DIFFICULTIES
