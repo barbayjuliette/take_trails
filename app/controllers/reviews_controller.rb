a7a21e81809d9f8b8e1615549c13c4916c480e14
Foo Xiaoxi <fooxiaoxi@gmail.com>
2022-11-24 23:20:09 +0800

    added users to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,13 @@ class ReviewsController < ApplicationController
   def index
     @trip = Trip.find(params[:trip_id])
     @reviews = Review.all
+
   end
 
   def create
     @trip = Trip.find(params[:trip_id])
     @review = Review.new(review_params)
+    @review.user = current_user
     @review.trip = @trip
     if @review.save
       redirect_to trip_path(@trip)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_044956) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_132411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_044956) do
     t.bigint "trip_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["trip_id"], name: "index_reviews_on_trip_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "trails", force: :cascade do |t|
@@ -68,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_044956) do
   end
 
   add_foreign_key "reviews", "trips"
+  add_foreign_key "reviews", "users"
   add_foreign_key "trips", "trails"
   add_foreign_key "trips", "users"
 end
