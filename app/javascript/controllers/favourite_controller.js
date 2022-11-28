f8501afc39f0d89cd93006516e8f087508aa2c97
juliette barbay <barbayjuliette95@gmail.com>
2022-11-28 17:08:32 +0800

    refactored

--- app/javascript/controllers/favourite_controller.js
+++ app/javascript/controllers/favourite_controller.js
@@ -5,15 +5,11 @@ export default class extends Controller {
   static values = {trail: Number}
 
   connect() {
-    console.log("inside")
   }
 
   bookmarked(event) {
     event.preventDefault();
-    console.log("bookmark clicked");
     const link = event.currentTarget
-
-    console.log(event.currentTarget.href);
 
     this.csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content");
 
@@ -23,19 +19,9 @@ export default class extends Controller {
     })
       .then(response => response.json())
       .then((data) => {
-        // find the div with this.trailValue
-        // console.log(document.querySelector(`div[data-favourite-trail-value="${this.trailValue}" i`))
         link.innerHTML = data
         ? `<i class="fa-regular fa-bookmark fw-bold"></i>`
         : `<i class="fa-regular fa-bookmark"></i>`
-
-        // find the i tag inside
-
-        // if data is true
-        // set i tag to have class fw-bold
-
-        // if data is false
-        // set i tag to have no class fw-bold
       })
   }
 }
