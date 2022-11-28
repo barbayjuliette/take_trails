6e6a86499813d745982820c72ffc497ba22e4ad1
Nurraihan Jalil <nurraihanjalilx@gmail.com>
2022-11-28 15:54:33 +0800

    redirect to trip show page instead of trips index page after creating a trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -11,7 +11,7 @@ class TripsController < ApplicationController
     trip_date = DateTime.parse("#{trip_params[:date]} +08:00")
     trip = Trip.new(user: current_user, trail: @trail, date: trip_date)
     if trip.save
-      redirect_to trips_path, notice: "Trip created!"
+      redirect_to trip_path(trip), notice: "Trip created!"
     else
       render 'trails/show', locals: {trip: trip}
     end
