27c5b557abb7ce8fd907b7e78a09b4403fa39bd8
Xing Ern Low <lxingern@gmail.com>
2022-11-29 09:58:48 +0800

    edit date format for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :trip
   belongs_to :user
-  # belongs_to :trail
   has_many_attached :photos
+
   validates :comment, length: { minimum: 10 }, presence: true
   validates :rating, presence: true
 end
