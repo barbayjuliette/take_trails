0dca1bb61eb8441777186a812d2cad45197921f5
Xing Ern Low <lxingern@gmail.com>
2022-11-30 10:10:57 +0800

    ajaxified overall rating after submission of review and some refactoring

--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -2,7 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="insert-in-list"
 export default class extends Controller {
-  static targets = ["items", "form", "blank", "count", "userreview"]
+  static targets = ["items", "form", "blank", "count", "userreview", "overallrating"]
+
+  connect() {
+    console.dir(this.overallratingTarget)
+  }
 
   send(event) {
     event.preventDefault()
@@ -14,54 +18,14 @@ export default class extends Controller {
   })
     .then(response => response.json())
     .then((data) => {
-      if (data.success) {
-        this.itemsTarget.insertAdjacentHTML("afterbegin", `
-         <div class="review">
-              <div class="user-info">
-                <i class="fa-solid fa-circle-user avatar"></i>
-              <div>
-                <h4>${data.reviewer}</h4>
-                <p>${data.created_at}</p>
-              </div>
-              <div class="rating">
-          <h4>${data.rating}<i class="fa-solid fa-star"></i> </h4>
-        </div>
-            </div>
-            <div class="reviews review-size">
-              ${data.review}
-            </div>
-          </div>
-        `);
-
-        this.userreviewTarget.insertAdjacentHTML("afterbegin", `
-        <div id="show-pg-review-card" class="review mb-2" style="width: 100%">
-          <div class="user-info">
-            <i id="show-pg-user-avatar" class="fa-solid fa-circle-user avatar"></i>
-            <div>
-              <h4>Your review</h4>
-              <p class="mt-0">${data.created_at}</p>
-            </div>
-            <div class="rating">
-              <h5>${data.rating}<i class="fa-solid fa-star"></i></h5>
-            </div>
-          </div>
-          <div class="reviews review-size">
-            ${data.review}
-          </div>
-        </div>
-        `);
-
-        document.querySelector("#no-review").classList.add('d-none');
-
+      // if (data.success) {
+        // in modal
         this.formTarget.innerHTML = `
           <div class=''>
-          <br>
+            <br>
             <h3 class="submitted">Thanks for submitting a review!</h3>
           </div>
         `
-
-        this.blankTarget.innerHTML = ``
-
         let newCount = 0
 
         if (this.countTarget.innerHTML) {
@@ -71,16 +35,23 @@ export default class extends Controller {
           this.countTarget.innerHTML = "1 review"
         }
 
-        document.querySelector("#main_review_count").innerHTML = (Number(newCount)+1) + " reviews"
+        this.itemsTarget.insertAdjacentHTML("afterbegin", data.reviewcard);
 
+        this.blankTarget.innerHTML = ``
 
-      } else {
-        console.log("FAILED")
+        // document.querySelector("#main_review_count").innerHTML = (Number(newCount)+1) + " reviews"
+
+        // on show page
+        document.querySelector("#no-review").classList.add('d-none');
+
+        this.userreviewTarget.insertAdjacentHTML("afterbegin", data.userreviewcard);
+
+        this.overallratingTarget.innerHTML = data.overallrating
       }
-      // if (data.inserted_item) {
-      //   this.itemsTarget.insertAdjacentHTML(this.positionValue, data.inserted_item)
-      // })
-      // this.formTarget.outerHTML = data.form
-    })
+      // else {
+      //   console.log("FAILED")
+      // }
+    // }
+    )
 }
 }
