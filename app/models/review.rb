b50a270edee6ac9680336e9ef14fe960ee897596
Foo Xiaoxi <fooxiaoxi@gmail.com>
2022-11-30 18:30:18 +0800

    removed line in reviews model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
   belongs_to :user
   has_many :tags, dependent: :destroy
   has_many :categories, through: :tags
-  belongs_to :trail
+
   has_many_attached :photos
 
   validates :comment, length: { minimum: 10 }, presence: true
